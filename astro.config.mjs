abcff0428adc829e46747164f8a9d1d122bd63f3
Grey <myusernamesuckssorry@gmail.com>
2026-02-27 14:36:33 -0330

    Further poke

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from 'astro/config';
 import awsAmplify from 'astro-aws-amplify';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
