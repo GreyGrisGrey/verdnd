ecc31bf3c6c010025e96a2c27442faac09e9d6de
Grey <myusernamesuckssorry@gmail.com>
2026-03-25 15:46:09 -0230

    Component system debugging

--- components.ts
+++ components.ts
@@ -10,11 +10,19 @@ function constructPage(pageName: string, objects: string[]) {
         .split('\r\n');
     let newOutput = '';
     for (const line of currFile) {
-        const args = line.split(' ');
+        const args = line.split('\t');
         if (args[0] === 'ADD') {
             newOutput += fs.readFileSync('./' + compDir + args[1], 'utf8');
         } else if (args[0] === 'INSERT') {
             newOutput += args[1];
+        } else if (args[0] === 'DIRADD') {
+            const toAdd = fs.readdirSync('./' + compDir + args[1]);
+            for (const newAdd of toAdd) {
+                newOutput += fs.readFileSync(
+                    './' + compDir + args[1] + newAdd,
+                    'utf8',
+                );
+            }
         }
     }
     fs.writeFileSync('./' + pageDir + newName, Buffer.from(newOutput, 'utf8'));
