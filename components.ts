e26e7fbab0b13ffca2c48ea45de72436282abc40
Grey <myusernamesuckssorry@gmail.com>
2026-03-25 15:29:12 -0230

    Added html component system, components.

---
+++ components.ts
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const compDir = '/components/';
+const formDir = '/formats/';
+const pageDir = '/pages/';
+
+function constructPage(pageName: string, objects: string[]) {
+    const newName = pageName.split('.')[0] + '.html';
+    const currFile = fs
+        .readFileSync('./' + formDir + pageName, 'utf8')
+        .split('\r\n');
+    let newOutput = '';
+    for (const line of currFile) {
+        const args = line.split(' ');
+        if (args[0] === 'ADD') {
+            newOutput += fs.readFileSync('./' + compDir + args[1], 'utf8');
+        } else if (args[0] === 'INSERT') {
+            newOutput += args[1];
+        }
+    }
+    fs.writeFileSync('./' + pageDir + newName, Buffer.from(newOutput, 'utf8'));
+}
+
+function constructPages() {
+    const toBuild = fs.readdirSync('./' + formDir);
+    const buildFrom = fs.readdirSync('./' + compDir);
+    for (const f of toBuild) {
+        constructPage(f, buildFrom);
+    }
+}
+
+constructPages();
--- construct.bat
+++ construct.bat
@@ -1,5 +1,7 @@
 call npx @biomejs/biome format --write
 python compile.py
+call tsc components.ts
+node components.js
 call tsc -p ./server/tsconfig.json
 call tsc -p tsconfig.json
 start http://47.55.46.138:4321/
